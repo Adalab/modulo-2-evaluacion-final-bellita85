ed5bd93887554fa91f8f3aeb2dd7870cd013bc10
Bella Irene <bellagarcia1985@gmail.com>
2022-11-13 14:13:25 +0100

    finish localstoragefav

--- src/js/0_variables.js
+++ src/js/0_variables.js
@@ -17,6 +17,16 @@ fetch('https://breakingbadapi.com/api/characters')
     paintcharacteres(characteres);
   });
 
+let savedFavourites = JSON.parse(localStorage.getItem('favoroutecharacteres'));
+//  con esto siempre SIEMPRE te traes la info pero ahora decides que hacer con ella con in if. te dice que cuando cargues la pagina me traigas la info guardada en localstorage, pero siempre hay que decirle que si no hay informacion pues nada, pero que si hay informacion ( !==null) osea que si es diferente a null pues que me pases esa lista a mi array de favourites y que me lo pintes nada mas arrancar
+if (savedFavourites !== null) {
+    favouriteCharacteres = savedFavourites;
+    paintcharacteresfav(favouriteCharacteres);
+}
+
+
+
+
 function paintcharacteres(paint) {
   for (const item of paint) {
     const liElement = document.createElement('li');
@@ -96,9 +106,30 @@ function handleClickFav(event) {
   const selectedId = characteres.find(
     (item) => item.char_id === parseInt(event.currentTarget.id)
   );
-  console.log(selectedId);
-  favouriteCharacteres.push(selectedId);
-  console.log(favouriteCharacteres);
+
+  const testerFavIndex = favouriteCharacteres.findIndex(
+    (item) => item.char_id === parseInt(event.currentTarget.id)
+  );
+  //    hazme un recorrido por todo favoritos y me traes Esto, pero no lo Pintes. luego lo uso para saber si me lo ha traido no me lo traigas mas. // seunda opcion es poner index que lo que me devuelve es solo la posicion del array en el que esta, pero que si no esta me da -1 asi me funciona tanto para pintar si es -1 como para quitar 1 en la posicion que yo diga que siempre sera indexof
+  if (testerFavIndex === -1) {
+    favouriteCharacteres.push(selectedId);
+    localStorage.setItem(
+      'favoroutecharacteres',
+      JSON.stringify(favouriteCharacteres)
+    );
+    // cada veaz que yo haga push en un favorito nuevo lo guardo en localstorege indicando un nombre q yo quiera y el contenido que quiero guardar pero ese contenido se lo tengo que pasar en texto y por eso el stringity. y lo mismo al usar splice.
+  }
+  // si testerfav ESTA en favoritos esta condicion no se ejecuta. solo se ejecuta si si terterfav es falsa, si no esta. // Y luego hemo smejorado la funcion con indexof para ponerle la concicion de si es -1 es que no esta, po lo cual lo metes. si tester es -1 es que al hacerme el barrido no esta, polo cual lo metes.
+  else {
+    favouriteCharacteres.splice(testerFavIndex, 1);
+    // y si terster si esta me das su posicion del array para quitarlo.  aqui esta diciendo que empieza en testerindex que es una posicion del array, y el segundo parametro es cuantos me tiene que quitar, en este caso 1.
+    localStorage.setItem(
+      'favoroutecharacteres',
+      JSON.stringify(favouriteCharacteres)
+    );
+  }
+  // si no esta lo meto si si esta lo quito
+
   paintcharacteresfav(favouriteCharacteres);
 }
 
